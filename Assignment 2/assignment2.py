e683526f07620fb1f2dc0011b2bf0670aa536e3a
jgray <j.gray@st.hanze.nl>
2024-09-26 17:24:29 +0200

    assignment2

--- Assignment 2/assignment2.py
+++ Assignment 2/assignment2.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 import argparse
+from mpi4py import MPI
+import trapezoid
+import numpy as np
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
 def parse_arguments():
     """
@@ -20,9 +27,30 @@ def parse_arguments():
         '-n', type=int, required=True,
         help="Number of steps for integration [int > 0]"
     )
-    parser.add_argument(
-        '-p', type=int, required=True,
-        help="Number of OpenMPI workers to start [int > 0]"
-    )
     return parser.parse_args()
 
+
+args = parse_arguments()
+
+lower_boundry = args.a
+upper_boundry = args.b
+
+if rank == 0:
+
+    intervals = np.linspace(lower_boundry, upper_boundry,
+                                                size+1)
+    boundries = [[intervals[i], intervals[i+1]] 
+                for i in range(len(intervals)-1)]
+    
+    comm.scatter(boundries)
+    results = comm.gather(None, root=0)
+    print(f"Results: {results}")
+else:
+    data = comm.scatter([],root=0)
+    print(data)
+    I = trapezoid.trapezoid_rule(func=trapezoid.function_to_integrate,
+                       lower_bound=lower_boundry,
+                       upper_bound=upper_boundry,
+                       num_steps=30)
+    I = data
+    comm.gather(I, root=0)
