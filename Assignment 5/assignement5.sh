6af12eb0cf07727808901e92ce37bc850a8fe556
jgray <j.gray@st.hanze.nl>
2025-03-12 11:31:15 +0100

    adjusted sbatch

--- Assignment 5/assignement5.sh
+++ Assignment 5/assignement5.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 #SBATCH --nodes 1
 #SBATCH --job-name James_time_assignment
-time /homes/jgray/miniconda3/envs/progiv/bin/python -m cProfile -s time ~/Documents/Master/Periode\ 5/PROGV/Assignment\ 5/main.py > logs.txt
+time /homes/jgray/miniconda3/envs/progiv/bin/python -m cProfile -s time ~/Documents/Master/Periode\ 5/PROGV/Assignment\ 3/main.py > logs.txt
