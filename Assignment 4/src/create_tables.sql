f8e7719b117cb44208d751f75463ede8dfc0320c
jgray <j.gray@st.hanze.nl>
2025-03-12 13:48:18 +0100

    spark_setup

---
+++ Assignment 4/src/create_tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE proteins (
+    accession_number TEXT,
+    accession_version TEXT,
+    species_name TEXT,
+    taxid TEXT,
+    locus TEXT,
+    protein_id TEXT,
+    position TEXT,
+    product TEXT,
+    AA_seq TEXT,
+    PRIMARY KEY (accession_number)
+);
